33478a75a2a50aa2ff8fcba209fcaba03bba0654
RAHMI <rachid.rahmi@atos.net>
2022-01-31 22:40:41 +0100

    @PathVariable

--- src/main/java/com/spring/restfull/config/controller/MainController.java
+++ src/main/java/com/spring/restfull/config/controller/MainController.java
@@ -6,7 +6,10 @@ import com.spring.restfull.model.*;
 import java.util.ArrayList;
 import java.util.List;
 
+import javax.annotation.PostConstruct;
+
 import org.springframework.web.bind.annotation.GetMapping;
+import org.springframework.web.bind.annotation.PathVariable;
 import org.springframework.web.bind.annotation.RequestMapping;
 import org.springframework.web.bind.annotation.RestController;
 
@@ -14,22 +17,33 @@ import org.springframework.web.bind.annotation.RestController;
 @RequestMapping(value = "/main")
 public class MainController {
 	
+	private List<Student> liste = new ArrayList<Student>();
+	
 	@GetMapping(value = "/sport")
 	public String getMain() {
 		return "HI My Sport...";
 	}
-
-	@GetMapping(value = "/students")
-	public List<Student> getStudents(){
+	
+	@PostConstruct
+	public void start() {
+		
 		Student st1 = new Student(1, "Rachid", "0101010101");
 		Student st2 = new Student(2, "Amel",   "0202020202");
 		Student st3 = new Student(3, "ASSIA",  "0030303030");
 		Student st4 = new Student(4, "Aya",    "0404040404");
 		Student st5 = new Student(5, "Zouz",   "0505050505");
-		
-		 List<Student> liste = new ArrayList<Student>();
 		 liste.add(st1);liste.add(st2);
 		 liste.add(st3); liste.add(st4); liste.add(st5);
+		
+	}
+
+	@GetMapping(value = "/students")
+	public List<Student> getStudents(){
 		 return liste;
+	}
+	
+	@GetMapping(value= "/getStudent/{id}")
+	public Student getStudentById(@PathVariable("id") int id) { 
+		 return liste.get(id - 1);
 	}
 }
