728fb9691bb324d35e91d8b5d36fac4c5d8537f4
RAHMI <rachid.rahmi@atos.net>
2022-02-01 21:17:51 +0100

    Create StudentException

---
+++ src/main/java/com/spring/restfull/exception/StudentException.java
@@ -0,0 +1,14 @@
+package com.spring.restfull.exception;
+
+public class StudentException extends RuntimeException {
+
+	/**
+	 * 
+	 */
+	private static final long serialVersionUID = -6236283255360005744L;
+	
+	public StudentException(String message) {
+		super(message);
+	}
+
+}
